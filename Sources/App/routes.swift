54b2d69f024f0ef53cad83a2352cd88c6cbed2e2
hopeht909 <hopeht909@gmail.com>
2021-12-24 20:45:37 +0000

    server

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,6 @@
 import Vapor
 
+
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
@@ -8,4 +9,25 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("tasks") { req -> [TaskResponse] in
+        let data = [
+            TaskResponse(createdAt: "Wed Mar 30 2021", objective: "Get this to work"),
+            TaskResponse(createdAt: "Wed Mar 30 2021", objective: "Get this to work well")
+        ]
+        return data
+    }
+    
+    app.post("tasks"){ req -> TaskResponse in
+        
+        if let movie = try? req.content.decode(TaskResponse.self){
+            return movie
+        }
+        return TaskResponse(createdAt: "error", objective: "400")
+    }
+}
+   
+struct TaskResponse: Content {
+    var createdAt: String
+    var objective: String
 }
